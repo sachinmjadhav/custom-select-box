fe141f8d83bb96e95f49bd4aac4f50832bca41b5
Sachin Jadhav <34229084+theSachinJadhav@users.noreply.github.com>
2018-05-23 13:50:05 +0530

    Initial Commit
    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import SelectBox from "./features/select-box";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Custom Select Box using ReactJS</h1>
+        <div style={{ margin: "16px", position: "relative" }}>
+          <SelectBox
+            width={200}
+            name="country_id"
+            items={[
+              { value: "India", id: 1 },
+              { value: "United States", id: 2 },
+              { value: "United Kingdom", id: 3 },
+              { value: "Germany", id: 4 }
+            ]}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
